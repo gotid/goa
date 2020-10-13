5ea2ed1425781c027d0cce065f5c2a9865609cae
zs <asdf>
2020-10-13 10:50:55 +0800

    新增 map reduce

---
+++ lib/syncx/done_chan.go
@@ -0,0 +1,27 @@
+package syncx
+
+import (
+	"github.com/z-sdk/goa/lib/lang"
+	"sync"
+)
+
+type DoneChan struct {
+	done chan lang.PlaceholderType
+	once sync.Once
+}
+
+func NewDoneChan() *DoneChan {
+	return &DoneChan{
+		done: make(chan lang.PlaceholderType),
+	}
+}
+
+func (dc *DoneChan) Close() {
+	dc.once.Do(func() {
+		close(dc.done)
+	})
+}
+
+func (dc *DoneChan) Done() chan lang.PlaceholderType {
+	return dc.done
+}
